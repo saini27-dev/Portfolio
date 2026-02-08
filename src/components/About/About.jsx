7650d20b56ab166962a2f30175af9663ac74bd7f
Sourabh Saini <sourabhsaini2711@gmail.com>
2026-02-08 21:31:27 +0530

    Revert "Update profile picture to use favicon.jpg"
    This reverts commit d803dfe5a90171bbef50142632af63a7b6076502.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../public/favicon.jpg';
+import profileImage from '../../assets/profile.jpg';
 
 
 const About = () => {
